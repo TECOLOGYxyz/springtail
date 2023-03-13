d483bb3083afbb037c2181fda2f6dd2790992d38
Hjalte Mann <hjalte@me.com>
2023-03-13 12:46:32 +0100

    Script now outputs file with pixel counts

--- code/generateTrain.py
+++ code/generateTrain.py
@@ -26,7 +26,7 @@ import math
 
 #filePath = r'O:\Tech_zoo\candida\3rd sending - photos F. candida tes ends\x_20230127'
 
-filePath = r'O:\Tech_zoo\candida\3rd sending - photos F. candida tes ends\x_20230227'
+filePath = r'C:\Users\au309263\OneDrive - Aarhus Universitet\Desktop\candida\data\x_20230227\raw'
 
 filesOrig = [i for i in os.listdir(filePath) if i.endswith(".jpg")]
 filesWhite = [i for i in os.listdir(filePath) if i.endswith(".tif")]
@@ -36,24 +36,32 @@ print(f'Original files: {filesWhite}')
 
 def generateBB(origName, white):
     whiteBin = cv2.cvtColor(white, cv2.COLOR_BGR2GRAY)
-    print(whiteBin.shape)
+    print("whiteBin shape ", whiteBin.shape)
     whiteBinHeight = whiteBin.shape[0]
     whiteBinWidth = whiteBin.shape[1]
-    #whiteBin = cv2.imread(os.path.join(filePath, w), cv2.IMREAD_GRAYSCALE) # Read the white/black image
     _, binary = cv2.threshold(whiteBin, 225, 255, cv2.THRESH_BINARY_INV)
-    cv2.imwrite(os.path.join(filePath, "binaryImages", "binayImage_" + origName + ".jpg"), binary)
+    #cv2.imwrite(os.path.join(filePath, "binaryImages", "binayImage_" + origName + ".jpg"), binary)
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
     numContours = 0
 
-    with open(os.path.join(filePath, "data",  origName + ".txt"), 'w') as f:
-        with open(os.path.join(filePath, "area",  origName + "_area" + ".txt"), 'w') as k:
-
+    with open(os.path.join(filePath, "data",  "delete"  + origName + ".txt"), 'w') as f:
+        with open(os.path.join(filePath, "pixelCount",  origName + "_pixelCount" + ".txt"), 'w') as k:
+            i = 0
             for c in contours:
+                #print("Cnt area: ", cv2.contourArea(c))
                 M = cv2.moments(c)
                 x,y,w,h = cv2.boundingRect(c)
-                print("M area: ", M['m00'])
-                print("Cnt area: ", cv2.contourArea(c))
+                #print("M area: ", M['m00'])
+                #print("Cnt area: ", cv2.contourArea(c))
+                #circ = cv2.arcLength(c, True)
+                #print("Circ ", circ)
 
+                blackimg = np.zeros((whiteBinHeight, whiteBinWidth, 3), dtype = np.uint8)
+                cv2.drawContours(blackimg, [c], -1, (0, 255, 0), 1)
+                contourPixelCount = np.count_nonzero(blackimg)
+                print("contour pixel count ", contourPixelCount)
+                cv2.imwrite(os.path.join(filePath, "conts", "cont_" + str(i) + origName + ".jpg"), blackimg)
+                i += 1
 
                 #print("xywh", x, y, w, h)
                 #print(f'BB coordinates: {x}, {y}, {w}, {h}')
@@ -69,17 +77,10 @@ def generateBB(origName, white):
                 cv2.rectangle(white,(x,y),(x+w,y+h),(255,0,0),1)
                 
                 clas = "0"
-                # xYolo = round((x+w/2)/whiteBinWidth, 5)
-                # yYolo = round((y+h/2)/whiteBinHeight, 5)
-                # wYolo = round(w/whiteBinWidth, 5)
-                # hYolo = round(h/whiteBinHeight, 5)
-
-
                 xYolo = (x+w/2)/whiteBinWidth
                 yYolo = (y+h/2)/whiteBinHeight
                 wYolo = w/whiteBinWidth
                 hYolo = h/whiteBinHeight
-
 
                 wYolo += wYolo*0.1 # Expand yolo detections with 10%
                 hYolo += hYolo*0.1
@@ -108,37 +109,33 @@ def generateBB(origName, white):
                     hYolo += d
                     hYolo = hYolo
                 
-                print("YOLO: ", xYolo, yYolo, wYolo, hYolo)
-                
-                
                 xYolo = math.ceil(xYolo * 1000000.0) / 1000000.0
                 yYolo = math.ceil(yYolo * 1000000.0) / 1000000.0
                 wYolo = math.floor(wYolo * 1000000.0) / 1000000.0
                 hYolo = math.floor(hYolo * 1000000.0) / 1000000.0
 
-                print("YOLO rounded: ", xYolo, yYolo, wYolo, hYolo)
+                print("YOLO coordinates (rounded): ", xYolo, yYolo, wYolo, hYolo)
+                print("")
 
+                #f.write(f'{clas} {xYolo} {yYolo} {wYolo} {hYolo}\n')
+                k.write(f'{clas} {xYolo} {yYolo} {wYolo} {hYolo} {contourPixelCount}\n')
 
-                f.write(f'{clas} {xYolo} {yYolo} {wYolo} {hYolo}\n')
-                k.write(f'{clas} {xYolo} {yYolo} {wYolo} {hYolo} {cv2.contourArea(c)}\n')
+            #cv2.imwrite(os.path.join(filePath, "boxImages", "binayBoxes_" + origName + ".jpg"), white)
+            #print("Image saved")
 
-            cv2.imwrite(os.path.join(filePath, "boxImages", "binayBoxes_" + origName + ".jpg"), white)
-            print("Image saved")
-
-        with open(os.path.join(filePath, "data", "stats.txt"), 'a') as w:
-            w.write(f'{origName}, {len(contours)}\n')
+        # with open(os.path.join(filePath, "data", "stats.txt"), 'a') as w:
+        #     w.write(f'{origName}, {len(contours)}\n')
 
         print(len(contours), numContours)
-        print("Stats saved")
+        #print("Stats saved")
 
 
 for i in filesOrig:
     print(i)
     fn = os.path.splitext(os.path.basename(i))[0] # Get name without extension
-    shutil.copy(os.path.join(filePath, i), os.path.join(filePath, "data", fn + ".jpg"))
+    #shutil.copy(os.path.join(filePath, i), os.path.join(filePath, "data", fn + ".jpg"))
     for w in filesWhite:
         if w.startswith(fn):
-            #imW = cv2.imread(os.path.join(filePath, w), cv2.IMREAD_GRAYSCALE) # Read the white/black image
             imW = cv2.imread(os.path.join(filePath, w)) 
             generateBB(fn, imW)
   
